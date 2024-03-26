3fe840f94a91fb8909273829748efb3fded0b15a
Mayank <mayankanand2701@gmail.com>
2024-03-26 22:42:55 +0530

    Adding new Learning about HTTP Methods

--- Building HTTP Server/index.js
+++ Building HTTP Server/index.js
@@ -2,18 +2,19 @@ const http=require("http");
 const fs=require("fs");
 const url=require("url");
 
-
 const myServer=http.createServer((req,res)=>{
     // call back functions
     // console.log("New Request Recieved.");
     // console.log(req.headers);
     // console.log(req);
-    const log=`${Date.now()}: ${req.url} New Request Recieved! \n`;
+    const log=`${Date.now()}: ${req.method} ${req.url} New Request Recieved! \n`;
     const myUrl=url.parse(req.url,true);
-    console.log(myUrl);
+    // console.log(myUrl);
     fs.appendFile("log.txt",log,(err,data)=>{
         switch(myUrl.pathname){
-            case "/":res.end("Home Page");
+            case "/":
+                // res.end("Home Page");
+                if(req.method==="GET") res.end("Home Page");
                     break;
             case "/about":
                   const username=myUrl.query.name;
@@ -23,6 +24,14 @@ const myServer=http.createServer((req,res)=>{
                 const search=myUrl.query.search_query;
                 res.end("Here are your results for "+search);
                 break;
+            case "/signup":
+                if(req.method==="GET")res.end("This is the signup form");
+                else if(req.method==="POST")
+                {
+                    // DB Query
+                    res.end("Success !");
+                }
+
             default:res.end("404:Not Found");
         }
     });
---
+++ HTTP Methods/index.js
@@ -0,0 +1,15 @@
+// GET
+// When you want to get some data from the server 
+
+// POST
+// When you want to send and mutate some data in the server
+
+// PUT 
+// When you want to upload some file on the server
+
+// PATCH
+// When you want to change some data in the existing server
+
+// DELETE 
+// When you want to delete some data form the server
+
