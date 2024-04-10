96e4077f420d0f5ee419dbdc575786b751565c63
Mayank <mayankanand2701@gmail.com>
2024-04-10 20:21:09 +0530

    Updating new Learning

--- URL Shortner/index.js
+++ URL Shortner/index.js
@@ -2,6 +2,7 @@ const express=require("express");
 const urlRoute=require("./routes/url");
 const {connectToMongoDB}=require("./connect");
 const URL=require("./models/url");
+const staticRoute=require("./routes/staticRoutes");
 
 const app=express();
 const PORT=8001;
@@ -9,11 +10,42 @@ const PORT=8001;
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
 .then(()=>console.log("MongoDB Connected "));
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+const path=require("path");
+app.set("views",path.resolve("./views"));
+
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+// Routes for Server Side Rendering
+// app.get("/test",async(req,res)=>{
+//     const allUrls=await URL.find({});
+//     // return res.end("<h1>He From the Server </h1>");
+
+//     // return res.end(`
+//     // <html>
+//     //     <head></head>
+//     //     <body>
+//     //         <ol>
+//     //             ${allUrls.map(url=>`<li>${url.shortId} - ${url.redirectURL} - ${url.vistHistory.length}</li>`).join("")}
+//     //         </ol>
+//     //     </body>
+//     // </html>
+//     // `);
+
+//     res.render("home",{
+//         // sending variables
+//         urls:allUrls,
+//     });
+// });
+
 
 app.use("/url",urlRoute);
 
-app.use("/:shortId",async(req,res)=>{
+app.use("/",staticRoute);
+
+app.get("/url/:shortId",async(req,res)=>{
     const shortId=req.params.shortId;
     const entry=await URL.findOneAndUpdate({
         shortId,
@@ -22,8 +54,8 @@ app.use("/:shortId",async(req,res)=>{
             timestamp:Date.now(),
         },
     },
-});
-res.redirect(entry.redirectURL);
+ });
+ res.redirect(entry.redirectURL);
 });
 
 app.listen(PORT,()=>console.log(`Server Started at Port : ${PORT}`));
